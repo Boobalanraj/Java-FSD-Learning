76b6c4ff62790bf3f0747a3756875b9a9678f7f5
Boobalanraj <boobalan.raj30072001@gmail.com>
2022-07-19 15:03:12 +0530

    Type Casting

---
+++ Phase-1/Practice-1/ExpilitTypecasting.java
@@ -0,0 +1,15 @@
+package com.simplilearn.demo;
+
+public class ExpilitTypecasting {
+		
+		public static void main(String [] args) {
+			
+			double a= 23.67;
+			
+			int b=(int) a;  ///convert forcefully to int
+			
+			System.out.println("Converted Double "+a+" to int "+b);
+			
+		}
+
+	}
